d0bdb7120b6e20915fd53c835e5a35a65ff57608
Ali Rizvi <frontear4594@gmail.com>
2022-12-15 23:51:31 -0500

    create basic ui concept
    art credits in readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Primrose
 A proof-of-concept visual novel designed in python via [pygame](https://www.pygame.org).
 
+## Credit
+- Background image created by [Alper Emiroglu](https://www.artstation.com/marketplace/p/lDK3/anime-classroom)
+- Icon by xTourmaline
+
 ## License
 This project is licensed under the [GNU General Public License v3](https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)) &#8212; you may copy, distribute and modify the software as long as you track changes/dates in source files.
--- src/main.py
+++ src/main.py
@@ -1,2 +1,28 @@
+import os
+import pygame
+from pygame.locals import *
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    pygame.init()
+
+    pygame.display.set_mode((854, 480))
+    pygame.display.set_caption("Primrose")
+    pygame.display.set_icon(pygame.image.load(os.path.join("assets", "icon.png")))
+
+    screen = pygame.display.get_surface()
+
+    dialogue_box = pygame.image.load(os.path.join("assets", "dialogue_box.png"))
+    background = pygame.image.load(os.path.join("assets", "background.png"))
+
+    dialogue_box = pygame.transform.scale(dialogue_box, (dialogue_box.get_width() / (background.get_width() / screen.get_width()), dialogue_box.get_height() / (background.get_height() / screen.get_height())))
+    background = pygame.transform.scale(background, pygame.display.get_window_size())
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
+        screen.blit(background, (0, 0))
+        screen.blit(dialogue_box, ((background.get_width() - dialogue_box.get_width()) / 2, background.get_height() - dialogue_box.get_height() - 10))
+
+        pygame.display.update()
