f948b4fbd063aa2386328f9812f234a17315feb0
Ali Rizvi <frontear4594@gmail.com>
2022-12-17 23:28:59 -0500

    add text for dialogue_box
    i realize how much more complicated text would need to be. handling overflow for one, and finding a size that isn't impossibly large while also looking consistent across all types of dialogue.

--- src/main.py
+++ src/main.py
@@ -10,9 +10,12 @@ if __name__ == "__main__":
     pygame.display.set_icon(pygame.image.load(os.path.join("assets", "icon.png")))
 
     screen = pygame.display.get_surface()
+    font = pygame.font.SysFont(pygame.font.get_default_font(), 32)
 
     dialogue_box = pygame.image.load(os.path.join("assets", "dialogue_box.png"))
     background = pygame.image.load(os.path.join("assets", "background.png"))
+    character = pygame.image.load(os.path.join("assets", "character.png"))
+    generic_text = font.render("Why hello there wonderful reader, it's so nice to meet you!", True, (255, 255, 255))
 
     dialogue_box = pygame.transform.scale(dialogue_box, (dialogue_box.get_width() / (background.get_width() / screen.get_width()), dialogue_box.get_height() / (background.get_height() / screen.get_height())))
     background = pygame.transform.scale(background, pygame.display.get_window_size())
@@ -23,6 +26,9 @@ if __name__ == "__main__":
                 exit()
 
         screen.blit(background, (0, 0))
-        screen.blit(dialogue_box, ((background.get_width() - dialogue_box.get_width()) / 2, background.get_height() - dialogue_box.get_height() - 10))
+        screen.blit(character, ((screen.get_width() - character.get_width()) / 2, character.get_height() / 8))
+        screen.blit(dialogue_box, (x := (background.get_width() - dialogue_box.get_width()) / 2, y := background.get_height() - dialogue_box.get_height() - 10))
+        #screen.blit(generic_text, (x, y))
+        screen.blit(generic_text, (x + (dialogue_box.get_width() - generic_text.get_width()) / 2, y + (dialogue_box.get_height() - generic_text.get_height()) / 2))
 
         pygame.display.update()
