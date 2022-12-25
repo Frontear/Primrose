0e786a92b7b8aa06290705fc8c55f9bf42294292
Ali Rizvi <frontear4594@gmail.com>
2022-12-24 19:30:47 -0500

    clean up asset names, clean up code
    im considering the methodology. should all assets be scaled down at the beginning, when they are loaded into the system, or should they be scaled down as necessary for scenes? it may make more sense to scale them down once to prevent needing to waste time recalculating, and it would make a lot of sense from the dev standpoint when they get accurate width/height values for a scaled surface

--- src/main.py
+++ src/main.py
@@ -2,33 +2,53 @@ import os
 import pygame
 from pygame.locals import *
 
-if __name__ == "__main__":
+def main():
     pygame.init()
 
-    pygame.display.set_mode((854, 480))
-    pygame.display.set_caption("Primrose")
-    pygame.display.set_icon(pygame.image.load(os.path.join("assets", "icon.png")))
+    screen = setup_display()
+    font = pygame.font.SysFont(pygame.font.get_default_font(), 48)
 
-    screen = pygame.display.get_surface()
-    font = pygame.font.SysFont(pygame.font.get_default_font(), 32)
+    dialogue_box = load_image("dialogue_box.png")
+    background = load_image("background.png")
+    billy = pygame.transform.scale2x(load_image("billy.png"))
+    text = font.render("Why hello there wonderful reader, it's a pleasure to meet you!", True, (255, 255, 255))
 
-    dialogue_box = pygame.image.load(os.path.join("assets", "dialogue_box.png"))
-    background = pygame.image.load(os.path.join("assets", "background.png"))
-    character = pygame.image.load(os.path.join("assets", "character.png"))
-    generic_text = font.render("Why hello there wonderful reader, it's so nice to meet you!", True, (255, 255, 255))
+    scene = background.copy()
+    scene.blit(billy, ((background.get_width() - billy.get_width()) / 2, 0))
+    scene.blit(dialogue_box, (x := (background.get_width() - dialogue_box.get_width()) / 2, y := background.get_height() - dialogue_box.get_height() - 20))
+    """
+    scene_unscaled = x1, y1: 98, 592 | x2, y2: 1181, 627
+    scene_scaled = x1, y1: 65, 394 | x2, y2: 788, 418
+    dialogue_box = x1, y1: 58, 72 | x2, y2: 1141, 107
+    obsolete_code: x1, y1: 27, 350??
+    98 / 65 = 1280 / 854
+    65 = 98 / (1280 / 854)
+    """
+    scene.blit(text, (x + (dialogue_box.get_width() - text.get_width()) / 2, y + (dialogue_box.get_height() - text.get_height()) / 2)) # TODO: text scaled down is grainy
+    scene = pygame.transform.scale(scene, screen.get_size())
 
-    dialogue_box = pygame.transform.scale(dialogue_box, (dialogue_box.get_width() / (background.get_width() / screen.get_width()), dialogue_box.get_height() / (background.get_height() / screen.get_height())))
-    background = pygame.transform.scale(background, pygame.display.get_window_size())
+    update_screen = True
 
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
-                exit()
+                exit(0)
 
-        screen.blit(background, (0, 0))
-        screen.blit(character, ((screen.get_width() - character.get_width()) / 2, character.get_height() / 8))
-        screen.blit(dialogue_box, (x := (background.get_width() - dialogue_box.get_width()) / 2, y := background.get_height() - dialogue_box.get_height() - 10))
-        #screen.blit(generic_text, (x, y))
-        screen.blit(generic_text, (x + (dialogue_box.get_width() - generic_text.get_width()) / 2, y + (dialogue_box.get_height() - generic_text.get_height()) / 2))
+        screen.blit(scene, (0, 0))
 
-        pygame.display.update()
+        if update_screen:
+            pygame.display.update()
+            update_screen = False
+
+def setup_display():
+    pygame.display.set_mode((854, 480))
+    pygame.display.set_caption("Primrose")
+    pygame.display.set_icon(load_image("icon.png"))
+
+    return pygame.display.get_surface()
+
+def load_image(name):
+    return pygame.image.load(os.path.join("assets", name))
+
+if __name__ == "__main__":
+    main()
